4bf2eacee39b1a2bce61ae843975ab4e802cf446
Abhinav Chaitanya R <abhinavchaitanya6@gmail.com>
2025-07-20 04:25:18 +0530

    Created dockerfile

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,50 @@
 /// <reference types="vite/client" />
+
+// CSS modules
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
+
+// Image files
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ico' {
+  const src: string;
+  export default src;
+}
+
+// Vite environment variables
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly VITE_WEATHER_API_KEY: string;
+  // Add other environment variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
